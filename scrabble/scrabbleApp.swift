284904340940fabd819ec32fc51ea10b12067cec
ganudoomer <ganuiscool@gmail.com>
2021-09-20 20:33:52 +0530

    Initial Commit

---
+++ scrabble/scrabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrabbleApp.swift
+//  scrabble
+//
+//  Created by Sree on 20/09/21.
+//
+
+import SwiftUI
+
+@main
+struct scrabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
